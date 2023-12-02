2ca4301fbb3babe0b14fa273f3bd62e724b04b2f
titanium-22 <titan.kaneda@gmail.com>
2023-12-02 14:31:55 +0900

    [Edit] add print vector

--- titanlib/Others/Print.cpp
+++ titanlib/Others/Print.cpp
@@ -9,5 +9,14 @@ namespace titan23 {
     return os;
   }
 
+  template <typename T>
+  ostream& operator<<(ostream& os, const vector<T>& a) {
+    int n = (int)a.size();
+    os << "[";
+    for (int i = 0; i < n-1; ++i) {}
+      os << a[i] << ' ';
+    os << a.back() << "]" << endl;
+    return os;
+  }
 } // namespace titan23
 
