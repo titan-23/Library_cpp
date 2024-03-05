8d917a492f308e59baff40152e88c8e91b765af7
titanium-22 <titan.kaneda@gmail.com>
2024-03-05 22:34:28 +0900

    [Add] static set

---
+++ titan_cpplib/data_structures/static_set.cpp
@@ -0,0 +1,68 @@
+#include <vector>
+using namespace std;
+
+// StaticSet
+namespace titan23 {
+
+  template<typename T>
+  struct StaticSet {
+    vector<T> data;
+    int n;
+
+    StaticSet() {}
+    StaticSet(vector<T> &a) : data(a) {
+      sort(data.begin(), data.end());
+      data.erase(unique(data.begin(), data.end()), data.end());
+      n = (int)data.size();
+    }
+
+    T get(const int i) const {
+      return data[i];
+    }
+
+    // key以上で最小
+    optional<T> ge(const T &key) const {
+      if (key > data.back()) return nullopt;
+      int l = -1, r = n-1;
+      while (r - l > 1) {
+        int mid = (l + r) >> 1;
+        ((data[mid] >= key)? r : l) = mid;
+      }
+      return data[r];
+    }
+
+    // keyより大きくて最小
+    optional<T> gt(const T &key) const {
+      if (key >= data.back()) return nullopt;
+      int l = -1, r = n-1;
+      while (r - l > 1) {
+        int mid = (l + r) >> 1;
+        ((data[mid] > key)? r : l) = mid;
+      }
+      return data[r];
+    }
+
+    // key以下で最大
+    optional<T> le(const T &key) const {
+      if (key < data[0]) return nullopt;
+      int l = 0, r = n;
+      while (r - l > 1) {
+        int mid = (l + r) >> 1;
+        ((data[mid] <= key)? l : r) = mid;
+      }
+      return data[l];
+    }
+
+    // key未満で最大
+    optional<T> lt(const T &key) const {
+      if (key <= data[0]) return nullopt;
+      int l = 0, r = n;
+      while (r - l > 1) {
+        int mid = (l + r) >> 1;
+        ((data[mid] < key)? l : r) = mid;
+      }
+      return data[l];
+    }
+  };
+}
+
