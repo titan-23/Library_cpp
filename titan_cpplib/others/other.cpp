845c7802788ba5ff48fdd8b1d7d7c3c0dc879c6f
titanium-22 <titan.kaneda@gmail.com>
2024-03-05 22:34:50 +0900

    [Edit] others

---
+++ titan_cpplib/others/other.cpp
@@ -0,0 +1,9 @@
+template<typename T, typename S>
+T abs(const T &a, const S &b) { return a<b? b-a: a-b; }
+
+template<typename T, typename S>
+T max(const T &a, const S &b) { return a<b? b: a; }
+
+template<typename T, typename S>
+T min(const T &a, const S &b) { return a<b? a: b; }
+
--- titan_cpplib/others/print.cpp
+++ titan_cpplib/others/print.cpp
@@ -1,30 +1,54 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 // print
-namespace titan23 {
+// pair<K, V>
+template <typename K, typename V>
+ostream& operator<<(ostream& os, const pair<K, V>& p) {
+  os << "(" << p.first << ", " << p.second << ")";
+  return os;
+}
 
-  // pair<K, V>
-  template <typename K, typename V>
-  ostream& operator<<(ostream& os, const pair<K, V>& p) {
-    os << "(" << p.first << ", " << p.second << ")";
-    return os;
+// vector<T>
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& a) {
+  int n = (int)a.size();
+  os << "[";
+  for (int i = 0; i < n-1; ++i) {
+    os << a[i] << ", ";
   }
-
-  // vector<T>
-  template <typename T>
-  ostream& operator<<(ostream& os, const vector<T>& a) {
-    int n = (int)a.size();
-    os << "[";
-    for (int i = 0; i < n-1; ++i) {
-      os << a[i] << ", ";
-    }
-    if (n > 0) {
-      os << a.back();
-    }
-    os << "]";
-    return os;
+  if (n > 0) {
+    os << a.back();
   }
-} // namespace titan23
+  os << "]";
+  return os;
+}
 
+// set<T>
+template <typename T>
+ostream& operator<<(ostream& os, const set<T>& a) {
+  int n = (int)a.size();
+  os << "{";
+  for (const T &x: a) {
+    os << x;
+    if (x != *(--a.end())) {
+      os << ", ";
+    }
+  }
+  os << "}";
+  return os;
+}
+
+// map<K, V>
+template <typename K, typename V>
+ostream& operator<<(ostream& os, const map<K, V>& mp) {
+  int n = (int)mp.size();
+  os << "{";
+  auto it = mp.begin();
+  while (it != mp.end()) {
+    os << it->first << ": " << it->second;
+    ++it;
+    if (it != mp.end()) {
+      os << ", ";
+    }
+  }
+  os << "}";
+  return os;
+}
