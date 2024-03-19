4994269226038335eb80248dbc4d51006247984c
titanium-22 <titan.kaneda@gmail.com>
2024-03-20 00:44:43 +0900

    [Fix] expander

--- titan_cpplib/cpp_expander.py
+++ titan_cpplib/cpp_expander.py
@@ -1,93 +1,46 @@
-raise NotImplementedError
-
 # user settings --------------------------------------------------
 SLASH = '/'
 LIB_PATH = '/mnt/c/Users/titan/source/Library_cpp'
 # LIB_PATH = 'C:\\Users\\titan\\source\\Library_cpp'
 TO_LIB_PATH = '.'
-#  ----------------------------------------------------------------
+#  ---------------------------------------------------------------
 
 import sys
 import pyperclip
 import re
 
-def get_code(now_path, input_file, need_class, is_input=False):
+def print_red(arg):
+  print(f'\u001b[31m{arg}\u001b[0m')
+
+def get_code(now_path, input_file, is_input=False):
   global input_lines, output
 
   for line in input_file:
-
-    if 'class' in line:
-      match = re.search(r'class\s+(\w+)', line)
-      if match:
-        class_name = match.group(1)
-        if class_name in need_class:
-          print(f'import {class_name} OK.')
-          need_class.remove(class_name)
-    if 'def' in line:
-      for func_name in need_class:
-        if line.startswith(f'def {func_name}'):
-          print(f'import {func_name} OK.(function)')
-          need_class.remove(func_name)
-
     if is_input:
       input_lines += 1
-    if line.startswith('from titan_pylib'):
-      _, s, _, *fs = line.split()
-      fs = [x.rstrip(', ') for x in fs]
-      s = s.replace('.', SLASH)
-      s = f'{LIB_PATH}{SLASH}{s}.py'
+    if line.startswith(f'#include "titan_cpplib{SLASH}'):
+      _, s = line.split()
+      s = s.replace('\"', '')
+      s = s.replace('\'', '')
+      s = f'{LIB_PATH}{SLASH}{s}'
       if s not in added_file:
-        output += f'# {line}'
+        output += f'// {line}'
         try:
           f = open(s, 'r', encoding='utf-8')
         except FileNotFoundError:
-          print(s)
           print(f'File \"{input_filename}\", line {input_lines}')
           error_line = line.rstrip()
-          error_underline = '^' * len(error_line)
-          print(f'    {error_line}')
-          print(f'    {error_underline}')
-          print('ImportError')
+          error_underline = '^' + '~' * (len(error_line)-1)
+          print_red(f'    {error_line}')
+          print_red(f'    {error_underline}')
+          print(f"FileNotFoundError")
           exit(1)
-        get_code(s, f, fs)
-        f.close()
-        added_file.add(s)
-    elif line.startswith('from .'):
-      output += f'# {line}'
-      _, s, _, *fs = line.split()
-      fs = [x.rstrip(', ') for x in fs]
-      cnt = 0
-      while s and s[0] == '.':
-        s = s[1:]
-        cnt += 1
-      s = s.replace('.', SLASH)
-      t = now_path
-      for _ in range(cnt):
-        i = len(t)-1
-        while i >= 0 and t[i] != SLASH:
-          i -= 1
-        t = t[:i]
-      s = f'{t}{SLASH}{s}.py'
-      if s not in added_file:
-        try:
-          f = open(s, 'r', encoding='utf-8')
-        except FileNotFoundError:
-          print(s)
-          print('FileNotFoundError')
-          exit(1)
-        get_code(s, f, fs)
+        get_code(s, f)
+        print(f"#include \"{s.replace(LIB_PATH, '')}\" OK.")
         f.close()
         added_file.add(s)
     else:
       output += line
-      # print(line, end='', file=output_file)
-  if need_class:
-    error_msg = ''
-    for e in need_class:
-      error_msg += f'  {e}\n'
-    error_msg += 'ImportError: class not found.'
-    print(error_msg)
-    exit(1)
 
 if __name__ == '__main__':
 
@@ -99,7 +52,7 @@ if __name__ == '__main__':
   output = ''
   input_lines = 0
 
-  get_code(TO_LIB_PATH, input_file, [], is_input=True)
+  get_code(TO_LIB_PATH, input_file, is_input=True)
 
   if output_filename in ['clip', 'CLIP', 'c', 'C']:
     output_filename = 'clipboard'
