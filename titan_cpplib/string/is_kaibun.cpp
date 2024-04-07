273af96dc3554c4ab0f18f255ff43b22d158f59c
titanium-22 <titan.kaneda@gmail.com>
2024-04-08 02:23:28 +0900

    fix

--- titan_cpplib/string/is_kaibun.cpp
+++ titan_cpplib/string/is_kaibun.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 
 bool is_kaibun(string &s) {
-  int n = (int)s.size();
-  for (int i = 0; i < n/2; ++i) {
-    if (s[i] != s[n-i-1]) return false;
+  for (int i = 0; i < s.size()/2; ++i) {
+    if (s[i] != s[s.size()-i-1]) return false;
   }
   return true;
 }
