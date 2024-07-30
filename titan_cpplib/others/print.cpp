239933f30a496ca5a0870a17118beddfe7b594a7
titanium-22 <titan.kaneda@gmail.com>
2024-07-30 16:53:56 +0900

    test

--- titan_cpplib/others/print.cpp
+++ titan_cpplib/others/print.cpp
@@ -5,10 +5,18 @@
 using namespace std;
 
 // print
+
 // pair<K, V>
 template <typename K, typename V>
 ostream& operator<<(ostream& os, const pair<K, V>& p) {
   os << "(" << p.first << ", " << p.second << ")";
+  return os;
+}
+
+// tuple<T1, T2, T3>
+template<typename T1, typename T2, typename T3>
+ostream &operator<<(ostream &os, const tuple<T1, T2, T3> &t) {
+  os << "( " << get<0>(t) << ", " << get<1>(t) << ", " << get<2>(t) << " )";
   return os;
 }
 
@@ -22,6 +30,18 @@ ostream& operator<<(ostream& os, const vector<T>& a) {
   }
   if (n > 0) {
     os << a.back();
+  }
+  os << "]";
+  return os;
+}
+
+// vector<vector<T>>
+template <typename T>
+ostream& operator<<(ostream& os, const vector<vector<T>>& a) {
+  os << "[\n";
+  int h = (int)a.size();
+  for (int i = 0; i < h; ++i) {
+    os << "  " << a[i] << '\n';
   }
   os << "]";
   return os;
