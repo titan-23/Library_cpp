61a1d9464c3d29acc1d25e7e202f6e92aa657ed7
titanium-22 <titan.kaneda@gmail.com>
2024-08-09 05:55:19 +0900

    fix indent

--- titan_cpplib/ahc/beam_search/action.cpp
+++ titan_cpplib/ahc/beam_search/action.cpp
@@ -2,33 +2,32 @@
 #include <cassert>
 using namespace std;
 
-// beam_search
+// Action
 namespace titan23 {
 
 namespace beam_search {
 
-  // 自由に変更してください!
-  enum class Action { U, R, D, L };
-  ostream& operator<<(ostream& os, const Action &action) {
-    switch (action) {
-      case Action::U: os << 'U'; break;
-      case Action::R: os << 'R'; break;
-      case Action::D: os << 'D'; break;
-      case Action::L: os << 'L'; break;
-      default: assert(false);
+    // 自由に変更してください!
+    enum class Action { U, R, D, L };
+    ostream& operator<<(ostream& os, const Action &action) {
+        switch (action) {
+            case Action::U: os << 'U'; break;
+            case Action::R: os << 'R'; break;
+            case Action::D: os << 'D'; break;
+            case Action::L: os << 'L'; break;
+            default: assert(false);
+        }
+        return os;
     }
-    return os;
-  }
 
-  Action get_rev_action(const Action &action) {
-    switch (action) {
-      case Action::U: return Action::D;
-      case Action::D: return Action::U;
-      case Action::R: return Action::L;
-      case Action::L: return Action::R;
+    Action get_rev_action(const Action &action) {
+        switch (action) {
+            case Action::U: return Action::D;
+            case Action::D: return Action::U;
+            case Action::R: return Action::L;
+            case Action::L: return Action::R;
+        }
+        assert(false);
     }
-    assert(false);
-  }
-
 }
-}
+}  // namespace titan23
