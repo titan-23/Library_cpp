2a3c6b5029cc8e4b6851e4d6cbbfe97da00a5423
titan-23 <titan.kaneda@gmail.com>
2024-10-27 23:51:52 +0900

    expanderコメント追加

--- titan_cpplib/cpp_expander.py
+++ titan_cpplib/cpp_expander.py
@@ -26,28 +26,38 @@ def to_green(arg):
     return f"\u001b[32m{arg}\u001b[0m"
 
 
-def init_clipboard():
-    for command in ["wl-clipboard", "xclip", "xsel"]:
-        if shutil.which(command):
-            pyperclip.set_clipboard(command)
-            break
-
-
 class CppExpander:
 
+    @staticmethod
+    def init_clipboard():
+        """pyperclipを日本語に対応させる"""
+        for command in ["wl-clipboard", "xclip", "xsel"]:
+            if shutil.which(command):
+                pyperclip.set_clipboard(command)
+                break
+
     def __init__(self, input_path: str) -> None:
+        """与えられた入力ファイルのパスをもつ`CppExpander`をインスタンス化する
+
+        Args:
+            input_path (str): 入力ファイルのパス
+        """
         if not os.path.exists(input_path):
             logger.critical(to_red(f'input_path : "{input_path}" does not exist.'))
+            logger.critical(to_red(f"FileNotFoundError"))
             exit(1)
         self.input_path: str = input_path
-        self.seen_path: set[str] = set()
         self.outputs: list[str] = []
         self.added_file: set[str] = set()
 
     def expand(self, output_fiepath: str) -> None:
-        """コードを展開してoutput_fiepathに書き出す"""
+        """コードを展開してoutput_fiepathに書き出す
+
+        Args:
+            output_fiepath (str): 出力ファイル / `clip`のとき、クリップボードに貼り付ける
+        """
         self.outputs = []
-        self.get_code(self.input_path)
+        self._get_code(self.input_path)
         output_code = "".join(self.outputs)
         if output_fiepath in ["clip"]:
             output_fiepath = "clipboard"
@@ -58,30 +68,34 @@ class CppExpander:
         logger.info(to_green("The process completed successfully."))
         logger.info(to_green(f'Output file: "{output_fiepath}".'))
 
-    def get_code(self, input_file_path: str) -> None:
-        input_lines = 0
+    def _get_code(self, input_file_path: str) -> None:
+        input_line_num = 0
         with open(input_file_path, "r", encoding="utf-8") as input_file:
             for line in input_file:
-                input_lines += 1
+                input_line_num += 1
                 if line.startswith(f'#include "titan_cpplib'):
                     _, s = line.split()
-                    s = s.replace('"', "")
-                    if s in self.added_file:
+                    target_file = s.replace('"', "")
+                    if target_file in self.added_file:
                         continue
-                    self.added_file.add(s)
+                    self.added_file.add(target_file)
                     for lib_path in LIB_PATH:
-                        new_path = f"{lib_path}{s}"
+                        new_path = f"{lib_path}{target_file}"
                         if os.path.exists(new_path):
                             self.outputs.append(f"// {line}")
-                            logger.info(f"[include] \"{s.replace(lib_path, '')}\"")
-                            self.get_code(new_path)
+                            logger.info(
+                                f"[include] \"{target_file.replace(lib_path, '')}\""
+                            )
+                            self._get_code(new_path)
                             break
                     else:
-                        logger.critical(f'File "{input_file_path}", line {input_lines}')
+                        logger.critical(
+                            f'File "{input_file_path}", line {input_line_num}'
+                        )
                         error_line = line.rstrip()
                         error_underline = "^" + "~" * (len(error_line) - 1)
-                        logger.critical(to_red(f"    {error_line}"))
-                        logger.critical(to_red(f"    {error_underline}"))
+                        logger.critical(to_red(f"\t{error_line}"))
+                        logger.critical(to_red(f"\t{error_underline}"))
                         logger.critical(to_red(f"FileNotFoundError"))
                         exit(1)
                 else:
@@ -96,7 +110,7 @@ if __name__ == "__main__":
         datefmt="%H:%M:%S",
         level=os.getenv("LOG_LEVEL", "INFO"),
     )
-    init_clipboard()
+    CppExpander.init_clipboard()
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
