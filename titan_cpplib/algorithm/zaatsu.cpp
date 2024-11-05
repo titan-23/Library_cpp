b9df12829e179763fec50ecf0a1628edb69056e8
titan-23 <titan.kaneda@gmail.com>
2024-11-06 02:04:10 +0900

    ｆ

--- titan_cpplib/algorithm/zaatsu.cpp
+++ titan_cpplib/algorithm/zaatsu.cpp
@@ -10,15 +10,15 @@ namespace titan23 {
     template<typename T>
     class Zaatsu {
       private:
-        vector<T> to_origin;
+        vector<T> _to_origin;
         int n;
 
       public:
         //! `used_items` からなる集合を管理するインスタンスを生成
-        Zaatsu(vector<T> &used_items) : to_origin(used_items) {
-            sort(to_origin.begin(), to_origin.end());
-            to_origin.erase(unique(to_origin.begin(), to_origin.end()), to_origin.end());
-            n = (int)to_origin.size();
+        Zaatsu(vector<T> &used_items) : _to_origin(used_items) {
+            sort(_to_origin.begin(), _to_origin.end());
+            _to_origin.erase(unique(_to_origin.begin(), _to_origin.end()), _to_origin.end());
+            n = (int)_to_origin.size();
         }
 
         //! 要素の種類数を返す(`max(to_zaatsu)`)
@@ -28,13 +28,13 @@ namespace titan23 {
 
         //! `x` を座標圧縮する
         int to_zaatsu(const T &x) const {
-            return lower_bound(to_origin.begin(), to_origin.end(), x) - to_origin.begin();
+            return lower_bound(_to_origin.begin(), _to_origin.end(), x) - _to_origin.begin();
         }
 
         //! 座標圧縮された `x` を戻す
         T to_origin(const int &x) const {
             assert(0 <= x && x < n);
-            return to_origin[x];
+            return _to_origin[x];
         }
     };
 }  // namespace titan23
