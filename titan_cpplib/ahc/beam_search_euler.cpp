89f3e406908b0783a67a92f558f1173df500c021
titanium-22 <titan.kaneda@gmail.com>
2025-04-19 04:17:39 +0900

    ビムサ改善

--- titan_cpplib/ahc/beam_search_euler.cpp
+++ titan_cpplib/ahc/beam_search_euler.cpp
@@ -11,16 +11,15 @@
 
 using namespace std;
 
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-
 //! 木上のビームサーチライブラリ
-namespace beam_search_with_tree {
+namespace flysquirrel { // flying squirrel over trees
 
 using ScoreType = long long;
 using HashType = unsigned long long;
-const ScoreType INF = 1e9;
+const ScoreType INF = 1e18;
 
 // Action
+// メモリ量は少ない方がよく、score,hash のメモは無くしたい
 struct Action {
     char d;
     ScoreType pre_score, nxt_score;
@@ -29,57 +28,81 @@ struct Action {
     Action() {}
     Action(const char d) : d(d), pre_score(INF), nxt_score(INF), pre_hash(0), nxt_hash(0) {}
 };
+
 ostream& operator<<(ostream& os, const Action &action) {
     os << action.d;
     return os;
 }
 
 class State {
-  private:
-
-  public:
+private:
+    titan23::Random srand;
     ScoreType score;
     HashType hash;
 
-    State() {}
-
+public:
     // TODO Stateを初期化する
-    void init() {}
+    void init() {
+        this->score = 0;
+        this->hash = 0;
+    }
 
     // TODO
     //! `action` をしたときの評価値とハッシュ値を返す
     //! ロールバックに必要な情報はすべてactionにメモしておく
-    pair<ScoreType, HashType> try_op(Action &action) const {}
+    pair<ScoreType, HashType> try_op(Action &action) const {
+        action.pre_score = score;
+        action.pre_hash = hash;
+        ScoreType nxt_score = score;
+        HashType nxt_hash = hash;
 
-    bool is_done() const {}
+        // TODO
+
+        action.nxt_score = nxt_score;
+        action.nxt_hash = nxt_hash;
+        return {nxt_score, nxt_hash};
+    }
 
     // TODO
     //! `action` をする
-    void apply_op(const Action &action) {}
+    void apply_op(const Action &action) {
+        // TODO
+        score = action.nxt_score;
+        hash = action.nxt_hash;
+    }
 
     // TODO
     //! `action` を戻す
-    void rollback(const Action &action) {}
+    void rollback(const Action &action) {
+        // TODO
+        score = action.pre_score;
+        hash = action.pre_hash;
+    }
 
     // TODO
     //! 現状態から遷移可能な `Action` の `vector` を返す
-    vector<Action> get_actions() const {}
+    vector<Action> get_actions() const {
+        vector<Action> actions;
+        return actions;
+    }
 
-    void print() const {}
+    void print() const {
+    }
 };
 
+
 struct BeamParam {
-    int MAX_TURN;
-    int BEAM_WIDTH;
+    int MAX_TURN, BEAM_WIDTH;
 };
 
 class BeamSearchWithTree {
     // ref: https://eijirou-kyopro.hatenablog.com/entry/2024/02/01/115639
 
-  private:
+private:
     static constexpr const int PRE_ORDER = -1;
     static constexpr const int POST_ORDER = -2;
-
+    titan23::Random rnd;
+    titan23::Timer beam_timer;
     titan23::HashSet seen;
     using ActionIDType = int;
     ActionIDType ActionID;
@@ -92,21 +115,23 @@ class BeamSearchWithTree {
     vector<tuple<int, Action, ActionIDType>> tree;
 
     // 次のビーム候補を保持する配列
-    vector<tuple<int, ScoreType, Action, ActionIDType>> next_beam; // <par, score, action, action_id>
+    vector<tuple<int, int, ScoreType, Action, ActionIDType>> next_beam; // <par, ch_dx, score, action, action_id>
 
     vector<vector<int>> next_beam_data;
 
     void get_next_beam(State* state, const int turn) {
         next_beam.clear();
-        next_beam.reserve(tree.size() * 4); // TODO
+        next_beam.reserve(tree.size()); // TODO
         seen.clear();
 
         if (turn == 0) {
             vector<Action> actions = state->get_actions();
+            int idx = 0;
             for (Action &action : actions) {
                 auto [score, hash] = state->try_op(action);
                 if (seen.contains_insert(hash)) continue;
-                next_beam.emplace_back(PRE_ORDER, score, action, ActionID);
+                next_beam.emplace_back(PRE_ORDER, idx, score, action, ActionID);
+                idx++;
                 ActionID++;
             }
             return;
@@ -119,10 +144,12 @@ class BeamSearchWithTree {
                 state->apply_op(action);
                 vector<Action> actions = state->get_actions();
                 std::get<0>(tree[i]) = leaf_id;
+                int idx = 0;
                 for (Action &action : actions) {
                     auto [score, hash] = state->try_op(action);
                     if (seen.contains_insert(hash)) continue;
-                    next_beam.emplace_back(leaf_id, score, action, ActionID);
+                    next_beam.emplace_back(leaf_id, idx, score, action, ActionID);
+                    idx++;
                     ActionID++;
                 }
                 ++leaf_id;
@@ -140,7 +167,7 @@ class BeamSearchWithTree {
         vector<tuple<int, Action, ActionIDType>> new_tree;
         new_tree.reserve(tree.size());
         if (turn == 0) {
-            for (auto &[par, _, new_action, action_id] : next_beam) {
+            for (auto &[par, _, __, new_action, action_id] : next_beam) {
                 assert(par == -1);
                 new_tree.emplace_back(0, new_action, action_id);
             }
@@ -170,7 +197,7 @@ class BeamSearchWithTree {
                 if (next_beam_data[dir_or_leaf_id].empty()) continue;
                 new_tree.emplace_back(PRE_ORDER, action, action_id);
                 for (const int beam_idx : next_beam_data[dir_or_leaf_id]) {
-                    auto &[_, __, new_action, new_action_id] = next_beam[beam_idx];
+                    auto &[_, __, ___, new_action, new_action_id] = next_beam[beam_idx];
                     new_tree.emplace_back(dir_or_leaf_id, new_action, new_action_id);
                 }
                 new_tree.emplace_back(POST_ORDER, action, action_id);
@@ -193,7 +220,7 @@ class BeamSearchWithTree {
     void get_result() {
         int best_id = -1;
         ScoreType best_score = 0;
-        for (auto [par, score, _, __] : next_beam) {
+        for (auto [par, ch_idx, score, _, __] : next_beam) {
             if (best_id == -1 || score < best_score) {
                 best_score = score;
                 best_id = par;
@@ -216,7 +243,7 @@ class BeamSearchWithTree {
         assert(false);
     }
 
-  public:
+public:
 
     /**
      * @brief ビームサーチをする
@@ -225,21 +252,23 @@ class BeamSearchWithTree {
      * @param verbose ログ出力するかどうか
      * @return vector<Action>
      */
-    vector<Action> search(const BeamParam &param, const bool verbose = false) {
+    vector<Action> search(const BeamParam &param, const bool verbose=false) {
+        beam_timer.reset();
         if (verbose) cerr << PRINT_GREEN << "Info: start search()" << PRINT_NONE << endl;
-
+        rnd = titan23::Random();
         ActionID = 0;
         State* state = new State;
         state->init();
 
-        this->seen = titan23::HashSet(param.BEAM_WIDTH * 4); // TODO
+        this->seen = titan23::HashSet(param.BEAM_WIDTH); // TODO
 
         int now_turn = 0;
         for (int turn = 0; turn < param.MAX_TURN; ++turn) {
-            if (verbose) cerr << "Info: # turn : " << turn+1 << endl;
+            if (verbose) cerr << "\nInfo: # turn : " << turn+1 << " | " << beam_timer.elapsed() << " ms" << endl;
 
             // 次のビーム候補を求める
             get_next_beam(state, turn-now_turn);
+            rnd.shuffle(next_beam); // シャッフルして多様性を確保(できているのか？)
 
             if (next_beam.empty()) {
                 cerr << PRINT_RED << "Error: \t次の候補が見つかりませんでした" << PRINT_NONE << endl;
@@ -250,18 +279,22 @@ class BeamSearchWithTree {
             int beam_width = min(param.BEAM_WIDTH, (int)next_beam.size());
             assert(beam_width <= param.BEAM_WIDTH);
 
-            nth_element(next_beam.begin(), next_beam.begin() + beam_width, next_beam.end(), [&] (const tuple<int, ScoreType, Action, ActionIDType> &left, const tuple<int, ScoreType, Action, ActionIDType> &right) {
-                return std::get<1>(left) < std::get<1>(right);
+            nth_element(next_beam.begin(), next_beam.begin() + beam_width, next_beam.end(), [&] (const tuple<int, int, ScoreType, Action, ActionIDType> &left, const tuple<int, int, ScoreType, Action, ActionIDType> &right) {
+                if (std::get<2>(left) == std::get<2>(right)) {
+                    // 評価値が同じ場合、各親から何番目のアクションかを比較して多様性を確保する
+                    return std::get<1>(left) < std::get<1>(right);
+                }
+                return std::get<2>(left) < std::get<2>(right);
             });
 
-            tuple<int, ScoreType, Action, ActionIDType> bests = *min_element(next_beam.begin(), next_beam.begin() + beam_width, [&] (const tuple<int, ScoreType, Action, ActionIDType> &left, const tuple<int, ScoreType, Action, ActionIDType> &right) {
-                return std::get<1>(left) < std::get<1>(right);
+            tuple<int, int, ScoreType, Action, ActionIDType> bests = *min_element(next_beam.begin(), next_beam.begin() + beam_width, [&] (const tuple<int, int, ScoreType, Action, ActionIDType> &left, const tuple<int, int, ScoreType, Action, ActionIDType> &right) {
+                return std::get<2>(left) < std::get<2>(right);
             });
-            if (verbose) cerr << "Info: \tbest_score = " << std::get<1>(bests) << endl;
-            if (std::get<1>(bests) == 0) { // TODO 終了条件
+            if (verbose) cerr << "Info: \tbest_score = " << std::get<2>(bests) << endl;
+            if (std::get<2>(bests) == 0) { // TODO 終了条件:ベストスコアを元に判定している
                 cerr << PRINT_GREEN << "Info: find valid solution." << PRINT_NONE << endl;
                 get_result();
-                result.emplace_back(std::get<2>(bests));
+                result.emplace_back(std::get<3>(bests));
                 return result;
             }
 
@@ -271,7 +304,7 @@ class BeamSearchWithTree {
                     next_beam_data.resize(next_beam.size());
                 }
                 for (int i = 0; i < beam_width; ++i) {
-                    auto &[par, _, new_action, new_action_id] = next_beam[i];
+                    auto &[par, _, __, new_action, new_action_id] = next_beam[i];
                     next_beam_data[par].emplace_back(i);
                 }
             }
@@ -286,18 +319,3 @@ class BeamSearchWithTree {
     }
 };
 } // namespace beam_search
-
-// int main() {
-//     beam_search_with_tree::BeamParam param;
-//     param.MAX_TURN = 1000;
-//     param.BEAM_WIDTH = 100;
-//     beam_search_with_tree::init_zhs();
-//     beam_search_with_tree::BeamSearchWithTree bs;
-//     vector<beam_search_with_tree::Action> ans = bs.search(param, true);
-//     for (const beam_search_with_tree::Action &action : ans) {
-//         cout << action;
-//     }
-//     cout << endl;
-//     cerr << "Score = " << ans.size() << endl;
-//     return 0;
-// }
