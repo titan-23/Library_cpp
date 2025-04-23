1f8b7d500e4b2032f4486cd597601a231dee804c
titanium-22 <titan.kaneda@gmail.com>
2025-04-23 11:53:36 +0900

    Fix indent

--- titan_cpplib/math/divs.cpp
+++ titan_cpplib/math/divs.cpp
@@ -1,45 +1,47 @@
 #include <iostream>
 #include <set>
 #include <vector>
+#include <algorithm>
 #include <map>
 using namespace std;
 
 // Osa_k
 namespace titan23 {
 
-  struct Osa_k {
-    int n;
+class Osa_k {
+private:
     vector<int> min_factor;
 
-    Osa_k(const int n) : n(n), min_factor(n+1) {
-      iota(min_factor.begin(), min_factor.end(), 0);
-      for (int i = 2; i*i <= n; ++i) {
-        if (min_factor[i] == i) {
-          for (int j = 2; j <= n/i; ++j) {
-            if (min_factor[i*j] > i) {
-              min_factor[i*j] = i;
+public:
+    Osa_k(const int n) : min_factor(n+1) {
+        iota(min_factor.begin(), min_factor.end(), 0);
+        for (int i = 2; i*i <= n; ++i) {
+            if (min_factor[i] == i) {
+                for (int j = 2; j <= n/i; ++j) {
+                        if (min_factor[i*j] > i) {
+                        min_factor[i*j] = i;
+                    }
+                }
             }
-          }
         }
-      }
     }
 
     vector<int> p_factorization(int n) const {
-      vector<int> ret;
-      while (n > 1) {
-        ret.emplace_back(min_factor[n]);
-        n /= min_factor[n];
-      }
-      return ret;
+        vector<int> ret;
+        while (n > 1) {
+            ret.emplace_back(min_factor[n]);
+            n /= min_factor[n];
+        }
+        return ret;
     }
 
     map<int, int> p_factorization_map(int n) const {
-      map<int, int> ret;
-      while (n > 1) {
-        ++ret[min_factor[n]];
-        n /= min_factor[n];
-      }
-      return ret;
+        map<int, int> ret;
+        while (n > 1) {
+            ++ret[min_factor[n]];
+            n /= min_factor[n];
+        }
+        return ret;
     }
-  };
+};
 }  // namespace titan23
--- titan_cpplib/string/is_kaibun.cpp
+++ titan_cpplib/string/is_kaibun.cpp
@@ -1,9 +1,9 @@
 #include <string>
 using namespace std;
 
-bool is_kaibun(string &s) {
-  for (int i = 0; i < s.size()/2; ++i) {
-    if (s[i] != s[s.size()-i-1]) return false;
-  }
-  return true;
+bool is_kaibun(const string &s) {
+    for (int i = 0; i < s.size()/2; ++i) {
+        if (s[i] != s[s.size()-i-1]) return false;
+    }
+    return true;
 }
