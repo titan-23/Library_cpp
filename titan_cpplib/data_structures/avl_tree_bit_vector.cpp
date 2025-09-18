bf7e78d78e33e116f85a140c6614e1c6f0d9cd45
titanium-22 <titan.kaneda@gmail.com>
2025-09-18 21:01:54 +0900

    A

--- titan_cpplib/data_structures/avl_tree_bit_vector.cpp
+++ titan_cpplib/data_structures/avl_tree_bit_vector.cpp
@@ -5,34 +5,11 @@
 #include <tuple>
 #include <nmmintrin.h>
 #include <stdint.h>
+#include "titan_cpplib/data_structures/fast_stack.cpp"
 using namespace std;
 
 // AVLTreeBitVector
 namespace titan23 {
-
-template<typename T>
-class FastStack {
-private:
-    vector<T> s;
-    size_t idx;
-
-public:
-    FastStack() : idx(0) {}
-    FastStack(size_t cap) : s(cap), idx(0) {}
-    bool empty() const { return idx == 0; }
-    void emplace(T v) {
-        if (idx < s.size()) {
-            s[idx] = v;
-        } else {
-            s.emplace_back(v);
-        }
-        idx++;
-    }
-    void clear() { idx = 0; }
-    void pop() { --idx; }
-    T top() const { return s[idx-1]; }
-    size_t size() const { return idx; }
-};
 
 class AVLTreeBitVector {
   private:
