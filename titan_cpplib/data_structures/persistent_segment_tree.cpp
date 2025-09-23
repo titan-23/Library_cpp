de3c4e480ca5da639e8f2d2fb652f44920e17443
titanium-22 <titan.kaneda@gmail.com>
2025-09-24 05:05:15 +0900

    葉木にした

--- titan_cpplib/data_structures/persistent_segment_tree.cpp
+++ titan_cpplib/data_structures/persistent_segment_tree.cpp
@@ -21,37 +21,28 @@ public:
         };
         // #pragma pack(pop)
 
-        // #pragma pack(push, 1)
-        struct Data {
-            T key, data;
-            Data() {}
-            Data(T k, T d) : key(k), data(d) {}
-        };
-        // #pragma pack(pop)
-
         vector<Node> tree;
-        vector<Data> data;
+        vector<T> data;
         size_t ptr, cap;
 
         MemoeyAllocator() : ptr(1), cap(1) {
             tree.emplace_back(0, 0);
-            data.emplace_back(T{}, T{});
+            data.emplace_back(e());
         }
 
         int copy(int node) {
-            int idx = new_node(data[node].key);
+            int idx = new_node(data[node]);
             tree[idx] = {tree[node].left, tree[node].right};
-            data[idx].data = data[node].data;
             return idx;
         }
 
         int new_node(const T key) {
             if (tree.size() > ptr) {
                 tree[ptr] = {0, 0};
-                data[ptr] = {key, key};
+                data[ptr] = key;
             } else {
                 tree.emplace_back(0, 0);
-                data.emplace_back(key, key);
+                data.emplace_back(key);
                 if (tree.size() >= cap) {
                     cap++;
                 }
@@ -82,18 +73,24 @@ private:
     int root;
     int _len;
 
+    int bit_length(int x) {
+        return x ? 32 - __builtin_clz(x) : 0;
+    }
+
     void update(int node) {
-        ma.data[node].data = ma.data[node].key;
-        if (ma.tree[node].left) ma.data[node].data = op(ma.data[ma.tree[node].left].data, ma.data[node].key);
-        if (ma.tree[node].right) ma.data[node].data = op(ma.data[node].data, ma.data[ma.tree[node].right].data);
+        ma.data[node] = op(ma.data[ma.tree[node].left], ma.data[ma.tree[node].right]);
     }
 
     void _build(vector<T> const &a) {
         auto build = [&] (auto &&build, int l, int r) -> int {
-            int mid = (l + r) >> 1;
-            int node = ma.new_node(a[mid]);
-            if (l != mid) ma.tree[node].left = build(build, l, mid);
-            if (mid+1 != r) ma.tree[node].right = build(build, mid+1, r);
+            if (r - l == 1) {
+                int node = ma.new_node(a[l]);
+                return node;
+            }
+            int mid = (l + r) / 2;
+            int node = ma.new_node(e());
+            ma.tree[node].left = build(build, l, mid);
+            ma.tree[node].right = build(build, mid, r);
             update(node);
             return node;
         };
@@ -117,32 +114,34 @@ private:
         if (l == r) return e();
         auto dfs = [&] (auto &&dfs, int node, int left, int right) -> T {
             if (right <= l || r <= left) return e();
-            if (l <= left && right < r) return ma.data[node].data;
+            if (l <= left && right <= r) return ma.data[node];
             int mid = (left + right) / 2;
             T res = e();
-            if (ma.tree[node].left) res = dfs(dfs, ma.tree[node].left, left, mid);
-            if (l <= mid && mid < r) res = op(res, ma.data[node].key);
-            if (ma.tree[node].right) res = op(res, dfs(dfs, ma.tree[node].right, mid+1, right));
+            if (ma.tree[node].left) {
+                res = dfs(dfs, ma.tree[node].left, left, mid);
+            }
+            if (ma.tree[node].right) {
+                res = op(res, dfs(dfs, ma.tree[node].right, mid, right));
+            }
             return res;
         };
         return dfs(dfs, root, 0, len());
     }
 
     vector<T> tovector() {
-        int node = root;
-        stack<int> s;
-        vector<T> a;
-        a.reserve(len());
-        while (!s.empty() || node) {
-            if (node) {
-                s.emplace(node);
-                node = ma.tree[node].left;
-            } else {
-                node = s.top(); s.pop();
-                a.emplace_back(ma.data[node].key);
-                node = ma.tree[node].right;
+        if (len() == 0) return {};
+        vector<T> a(len());
+        a.resize(_len);
+        auto dfs = [&](auto &&dfs, int node, int l, int r) -> void {
+            if (r - l == 1) {
+                a[l] = ma.data[node];
+                return;
             }
-        }
+            int mid = (l + r) / 2;
+            dfs(dfs, ma.tree[node].left, l, mid);
+            dfs(dfs, ma.tree[node].right, mid, r);
+        };
+        dfs(dfs, root, 0, _len);
         return a;
     }
 
@@ -154,36 +153,28 @@ private:
         assert(0 <= k && k < len());
         int new_root = ma.copy(root);
         if (len() <= 1) {
-            ma.data[new_root].key = v;
-            ma.data[new_root].data = v;
+            ma.data[new_root] = v;
             return PSEG(new_root, len());
         }
         int node = new_root;
-        int pnode = 0;
         int l = 0, r = len();
-        int d = 0;
         path.clear(); path.emplace(node);
-        while (1) {
+        while (r - l > 1) {
+            int pnode = node;
             int mid = (l + r) / 2;
-            if (k == mid) break;
-            pnode = node;
             if (k < mid) {
                 node = ma.copy(ma.tree[pnode].left);
                 ma.tree[pnode].left = node;
                 r = mid;
-                d = 1;
             } else {
                 node = ma.copy(ma.tree[pnode].right);
                 ma.tree[pnode].right = node;
-                l = mid+1;
-                d = 0;
+                l = mid;
             }
             path.emplace(node);
         }
-        ma.data[node].key = v;
-        if (d) ma.tree[pnode].left = node;
-        else ma.tree[pnode].right = node;
-        update(node);
+        ma.data[node] = v;
+        path.pop();
         while (!path.empty()) {
             update(path.top());
             path.pop();
@@ -195,19 +186,17 @@ private:
         assert(0 <= k && k < len());
         int node = root;
         int l = 0, r = len();
-        while (1) {
+        while (r - l > 1) {
             int mid = (l + r) / 2;
-            if (k == mid) {
-                return ma.data[node].key;
-            }
             if (k < mid) {
                 node = ma.tree[node].left;
                 r = mid;
             } else {
                 node = ma.tree[node].right;
-                l = mid+1;
+                l = mid;
             }
         }
+        return ma.data[node];
     }
 
     void print() {
@@ -221,24 +210,22 @@ private:
     }
 
     // fromの[l, r)を自身の[l, r)にコピーしたものを返す(自身は不変)
-    PSEG copy_from(PSEG &from, int l, int r) {
+    PSEG copy_from(const PSEG &from, int l, int r) {
         assert(0 <= l && l <= r && r <= len());
-        auto dfs = [&] (auto &&dfs, int fr, int to, int left, int right) -> int {
-            if (!fr && !to) return fr;
-            to = ma.copy(to);
-            if (right <= l || r <= left) { return to; }
-            if (l <= left && right < r) { return fr; }
-            int mid = (left + right) / 2;
-            ma.tree[to].left = dfs(dfs, ma.tree[fr].left,  ma.tree[to].left,  left, mid);
-            if (l <= mid && mid < r) {
-                // data,sizeはこの後updateする
-                ma.data[to].key = ma.data[fr].key;
-            }
-            ma.tree[to].right = dfs(dfs, ma.tree[fr].right, ma.tree[to].right, mid+1, right);
-            update(to);
-            return to;
+        assert(from.len() == len());
+
+        auto dfs = [&](auto &&dfs, int fr, int to, int l, int r) -> int {
+            if (!to || r <= l || r <= l) return fr;
+            if (l <= l && r <= r) return to;
+            int new_node = ma.copy(fr);
+            int mid = (l + r) / 2;
+            ma.tree[new_node].left = dfs(dfs, ma.tree[fr].left, ma.tree[to].left, l, mid);
+            ma.tree[new_node].right = dfs(dfs, ma.tree[fr].right, ma.tree[to].right, mid, r);
+            update(new_node);
+            return new_node;
         };
-        int new_root = dfs(dfs, ma.copy(from.root), ma.copy(root), 0, len());
+
+        int new_root = dfs(dfs, root, from.root, 0, len());
         return PSEG(new_root, len());
     }
 
