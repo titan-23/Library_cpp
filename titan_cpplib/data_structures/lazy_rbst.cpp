d1d2421c08f5547ee4d02e42fc4bdc4371dd792d
titanium-22 <titan.kaneda@gmail.com>
2025-09-24 05:35:45 +0900

    compositionの量を減らした

--- titan_cpplib/data_structures/lazy_rbst.cpp
+++ titan_cpplib/data_structures/lazy_rbst.cpp
@@ -43,8 +43,10 @@ class LazyRBST {
         void push(F f) {
             key = mapping(f, key);
             data = mapping(f, data);
-            lazy = composition(f, lazy);
-            has_lazy = true;
+            if (left || right) {
+                lazy = composition(f, lazy);
+                has_lazy = true;
+            }
         }
 
         void propagate() {
@@ -324,96 +326,3 @@ FastStack<typename LazyRBST<T, op, e, F, mapping, composition, id>::Node*> LazyR
 template <class T, T (*op)(T, T), T (*e)(), class F, T (*mapping)(F, T), F (*composition)(F, F), F (*id)()>
 FastStack<typename LazyRBST<T, op, e, F, mapping, composition, id>::Node*> LazyRBST<T, op, e, F, mapping, composition, id>::path;
 } // namespace titan23
-
-
-
-
-// ------------------------
-
-#include <bits/stdc++.h>
-using ll = long long;
-#define rep(i, n) for (int i = 0; i < (ll)(n); ++i)
-
-static constexpr ll mod = 998244353;
-static constexpr ll bit = 30;
-static constexpr ll msk = (1<<bit)-1;
-
-ll op(const ll s, const ll t) {
-    ll s1 = s>>bit, s2 = s&msk;
-    ll t1 = t>>bit, t2 = t&msk;
-    ll c1 = (s1 + t1) % mod;
-    ll c2 = (s2 + t2) % mod;
-    return (c1 << 30) + c2;
-}
-
-ll mapping(const ll f, const ll s) {
-    ll f1 = f>>bit, f2 = f&msk;
-    ll s1 = s>>bit, s2 = s&msk;
-    return (((s1 * f1 + s2 * f2) % mod) << 30) + s2;
-}
-
-ll composition(const ll f, const ll g) {
-    ll f1 = f>>bit, f2 = f&msk;
-    ll g1 = g>>bit, g2 = g&msk;
-    ll z1 = (f1 * g1) % mod;
-    ll z2 = (f1 * g2 + f2) % mod;
-    return (z1 << 30) + z2;
-}
-
-ll e() {
-    return 0;
-}
-
-ll id() {
-    return 1<<bit;
-}
-
-void dynamic_sequence_range_affine_range_sum() {
-    int n, q;
-    cin >> n >> q;
-    vector<ll> A(n);
-    rep(i, n) {
-        ll a;
-        cin >> a;
-        A[i] = a<<bit|1ll;
-    }
-
-    using Tree = titan23::LazyRBST<ll, op, e, ll, mapping, composition, id>;
-    Tree s(A);
-
-    rep(qdx, q) {
-        int com; cin >> com;
-        if (com == 0) {
-            int i;
-            ll x;
-            cin >> i >> x;
-            s.insert(i, x<<bit|1ll);
-        } else if (com == 1) {
-            int i;
-            cin >> i;
-            s.pop(i);
-        } else if (com == 2) {
-            int l, r;
-            cin >> l >> r;
-            s.reverse(l, r);
-        } else if (com == 3) {
-            int l, r;
-            ll b, c;
-            cin >> l >> r >> b >> c;
-            s.apply(l, r, b<<bit|c);
-        } else {
-            int l, r;
-            cin >> l >> r;
-            ll res = s.prod(l, r)>>bit;
-            cout << res << '\n';
-        }
-    }
-    // cerr << s.height() << endl;
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    dynamic_sequence_range_affine_range_sum();
-    return 0;
-}
