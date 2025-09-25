5e8f8b4d28f4b0132d135ffb51e10b1b69f9a9c5
titanium-22 <titan.kaneda@gmail.com>
2025-09-25 22:33:32 +0900

    add 64tree

---
+++ titan_cpplib/data_structures/wordsize_tree_set.cpp
@@ -0,0 +1,178 @@
+#include <vector>
+#include <cassert>
+#include "titan_cpplib/others/print.cpp"
+using namespace std;
+
+namespace titan23 {
+
+class WordsizeTreeSet {
+private:
+    using u64 = unsigned long long;
+    static constexpr const int W = 64;
+    int u, _len;
+    vector<vector<u64>> data;
+
+    inline constexpr int bit_length(const u64 x) {
+        return x ? 64-__builtin_clzll(x) : 0;
+    }
+
+    void _build(int u, vector<u64> &A) {
+        data[0] = A;
+        int dep = 1;
+        while (u) {
+            data[dep] = vector<u64>((u>>6)+1);
+            const auto &a = data[dep-1];
+            for (int i = 0; i <= u; ++i) {
+                if (a[i]) {
+                    data[dep][i>>6] |= 1ull << (i&63);
+                }
+            }
+            dep++;
+            u >>= 6;
+        }
+    }
+
+public:
+    WordsizeTreeSet(int u) : u(u+1), _len(0) {
+        while (u) {
+            u >>= 6;
+            data.emplace_back(u+1);
+        }
+    }
+
+    WordsizeTreeSet(int u, const string &s) : u(u+1), _len(0) {
+        u >>= 6;
+        int x = u, dep = 1;
+        while (x) { x >>= 6; dep++; }
+        data.resize(dep);
+        vector<u64> A(u+1);
+        u64 n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == '1') {
+                ++_len;
+                A[i>>6] |= 1ull << (i&63);
+            }
+        }
+        _build(u, A);
+    }
+
+    WordsizeTreeSet(int u, const vector<int> &a) : u(u+1), _len(0) {
+        u >>= 6;
+        int x = u, dep = 1;
+        while (x) {
+            x >>= 6; dep++;
+        }
+        data.resize(dep);
+        vector<u64> A(u+1);
+        for (int e : a) {
+            if (((A[e>>6] >> (e&63)) & 1) == 0) {
+                ++_len;
+                A[e>>6] |= 1ull << (e&63);
+            }
+        }
+        _build(u, A);
+    }
+
+    bool add(int v) {
+        assert(0 <= v && v < u);
+        if ((data[0][v>>6] >> (v&63)) & 1) return false;
+        _len++;
+        for (vector<u64> &a : data) {
+            a[v>>6] |= 1ull << (v&63);
+            v >>= 6;
+        }
+        return true;
+    }
+
+    bool discard(int v) {
+        if (((data[0][v >> 6] >> (v & 63) & 1)) == 0) return false;
+        _len--;
+        for (vector<u64> &a : data) {
+            a[v>>6] &= ~(1ull << (v & 63));
+            v >>= 6;
+            if (a[v]) break;
+        }
+        return true;
+    }
+
+    void remove(u64 v) {
+        discard(v);
+    }
+
+    int ge(u64 v) {
+        int d = 0;
+        while (1) {
+            if (d >= (int)data.size() || ((v>>6) >= data[d].size())) { return -1; }
+            u64 m = data[d][v>>6] & ((~(u64)0) << (v & 63));
+            if (m == 0) {
+                d++;
+                v = (v >> 6) + 1;
+            } else {
+                v = (v >> 6 << 6) + bit_length(m & -m);
+                if (v == 0) return -1;
+                v--;
+                if (d == 0) break;
+                v <<= 6;
+                d--;
+            }
+        }
+        return v;
+    }
+
+    int gt(u64 v) {
+        return ge(v+1);
+    }
+
+    int le(u64 v) {
+        int d = 0;
+        while (1) {
+            if (v < 0 || d >= (int)data.size()) return -1;
+            u64 m = data[d][v >> 6] & ~((~1ull) << (v & 63));
+            if (m == 0) {
+                d++;
+                if ((v>>6) == 0) return -1;
+                v = (v >> 6) - 1;
+            } else {
+                v = (v >> 6 << 6) + bit_length(m) - 1;
+                if (d == 0) break;
+                v <<= 6;
+                v += 63;
+                --d;
+            }
+        }
+        return v;
+    }
+
+    int lt(u64 v) {
+        return le(v+1);
+    }
+
+    bool contains(int v) {
+        return (data[0][v>>6] >> (v&63) & 1) == 1;
+    }
+
+    int len() { return _len; }
+
+    vector<int> tovector() {
+        int v = 0;
+        vector<int> a(_len);
+        int idx = 0;
+        {
+            if (contains(v)) {
+                a[0] = 0;
+                idx++;
+                v = gt(v);
+            }
+        }
+        while (1) {
+            int nxt = gt(v);
+            if (nxt == -1) break;
+            v = nxt;
+            a[idx] = 0;
+            idx++;
+        }
+        return a;
+    }
+};
+
+} // namespace titan23
