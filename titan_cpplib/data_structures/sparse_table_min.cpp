a288476cd60c26508ac8fd5e18c4a15c726e95a1
titanium-22 <titan.kaneda@gmail.com>
2025-09-26 22:36:27 +0900

    a

--- titan_cpplib/data_structures/sparse_table_min.cpp
+++ titan_cpplib/data_structures/sparse_table_min.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
+#include <immintrin.h>
 using namespace std;
 
 // SparseTableMin
@@ -9,36 +10,43 @@ namespace titan23 {
 struct SparseTableMin {
 private:
     int n;
-    vector<vector<int>> data;
+    vector<int> data, offset;
 
 public:
     SparseTableMin() {}
     SparseTableMin(vector<int> &a) : n((int)a.size()) {
         int log = 32 - __builtin_clz(n) - 1;
-        data.resize(log+1);
-        data[0] = a;
+        offset.resize(log+1);
+        int sm = 0;
+        for (int i = 0; i <= log; ++i) {
+            offset[i] = sm;
+            sm += n - (1<<i) + 1;
+        }
+        data.resize(sm);
+        memcpy(data.data(), a.data(), n*sizeof(int));
         for (int i = 0; i < log; ++i) {
-            const vector<int> &pre = data[i];
-            vector<int> &nxt = data[i+1];
             int l = 1 << i;
-            int s = pre.size();
-            nxt.resize(s-l);
-            for (int j = 0; j < s-l; ++j) {
-                nxt[j] = min(pre[j], pre[j+l]);
+            int s = n - l + 1;
+            const int x = s - l;
+            const auto pre1 = &data[offset[i]];
+            const auto pre2 = &data[offset[i]+l];
+            auto nxt = &data[offset[i+1]];
+            for (int j = 0; j < x; ++j) {
+                nxt[j] = min(pre1[j], pre2[j]);
             }
         }
     }
 
     int prod(const int l, const int r) const {
         assert(0 <= l && l <= r && r < n);
-        if (l == r) return -1;
+        if (l == r) return INT_MAX;
         int u = 32 - __builtin_clz(r-l) - 1;
-        return min(data[u][l], data[u][r-(1<<u)]);
+        return min(data[offset[u]+l], data[offset[u]+r-(1<<u)]);
     }
 
     int get(const int k) const {
         assert(0 <= k && k < n);
-        return data[0][k];
+        return data[k];
     }
 
     int len() const {
@@ -48,10 +56,10 @@ public:
     void print() const {
         cout << '[';
         for (int i = 0; i < n-1; ++i) {
-            cout << data[0][i] << ", ";
+            cout << data[i] << ", ";
         }
         if (n > 0) {
-            cout << data[0][n-1];
+            cout << data[n-1];
         }
         cout << ']' << endl;
     }
