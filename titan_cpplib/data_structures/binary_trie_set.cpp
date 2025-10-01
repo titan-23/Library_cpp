88f564409d2a7c5ac3baa3095a30d7046a2365f5
titan-23 <titan.kaneda@gmail.com>
2025-10-01 15:21:28 +0900

    Refactor bt

--- titan_cpplib/data_structures/binary_trie_set.cpp
+++ titan_cpplib/data_structures/binary_trie_set.cpp
@@ -1,84 +1,101 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
 
 namespace titan23 {
 
 template<typename T>
-struct BinaryTrieSet {
-    vector<unsigned int> left, right, par, size;
-    unsigned int _end, _root, _bit;
-    T _lim, _xor_val;
+class BinaryTrieSet {
+private:
+    int root, bit;
+    T lim, xor_val;
 
-    BinaryTrieSet(unsigned int bit) {
-        _end = 2;
-        _bit = bit;
-        _root = 1;
-        _lim = 1ll << bit;
-        _xor_val = 0;
-        left.resize(2);
-        right.resize(2);
-        par.resize(2);
-        size.resize(2);
-    }
+    struct MemoeyAllocator {
 
-    void reserve(const int n) {
-        left.reserve(n);
-        right.reserve(n);
-        par.reserve(n);
-        size.reserve(n);
-    }
+        struct Node {
+            int ch[2];
+            int par, size;
+            Node() : par(0), size(0) { ch[0] = ch[1] = 0; }
 
-    unsigned int _make_node() {
-        if (_end >= (int)left.size()) {
-            left.emplace_back(0);
-            right.emplace_back(0);
-            par.emplace_back(0);
-            size.emplace_back(0);
-        }
-        return _end++;
-    }
-
-    unsigned int _find(T key) const {
-        key ^= _xor_val;
-        unsigned int node = _root;
-        for (int i = _bit-1; i >= 0; --i) {
-            if ((key >> i) & 1) {
-                if (!right[node]) return -1;
-                node = right[node];
-            } else {
-                if (!left[node]) return -1;
-                node = left[node];
+            void clear() {
+                ch[0] = ch[1] = 0;
+                par = 0; size = 0;
             }
+        };
+
+        vector<Node> tree;
+        stack<int> erased;
+        size_t ptr;
+
+        MemoeyAllocator() : ptr(2) {
+            tree.emplace_back(Node{});
+            tree.emplace_back(Node{});
+        }
+
+        int new_node() {
+            if (!erased.empty()) {
+                int idx = erased.top(); erased.pop();
+                tree[idx].clear();
+                return idx;
+            }
+            if (tree.size() > ptr) {
+                tree[ptr].clear();
+            } else {
+                tree.emplace_back(Node{});
+            }
+            ptr++;
+            return ptr - 1;
+        }
+
+        void erase(int node) {
+            erased.emplace(node);
+        }
+
+        void reserve(int n) {
+            tree.reserve(n);
+        }
+    } ma;
+
+    inline int _find(T key) const {
+        key ^= xor_val;
+        int node = root;
+        for (int i = bit-1; i >= 0; --i) {
+            int c = key >> i & 1;
+            if (!ma.tree[node].ch[c]) return -1;
+            node = ma.tree[node].ch[c];
         }
         return node;
     }
 
+public:
+    BinaryTrieSet(int bit) : bit(bit) {
+        root = 1;
+        lim = (T)1 << bit;
+        xor_val = 0;
+    }
+
+    void reserve(const int n) {
+        ma.reserve(n);
+    }
+
     bool add(T key) {
-        key ^= _xor_val;
-        unsigned int node = _root;
-        for (int i = _bit-1; i >= 0; --i) {
-            if ((key >> i) & 1) {
-                if (!right[node]) {
-                    unsigned int new_node = _make_node();
-                    right[node] = new_node;
-                    par[right[node]] = node;
-                }
-                node = right[node];
-            } else {
-                if (!left[node]) {
-                    unsigned int new_node = _make_node();
-                    left[node] = new_node;
-                    par[left[node]] = node;
-                }
-                node = left[node];
+        key ^= xor_val;
+        int node = root;
+        for (int i = bit-1; i >= 0; --i) {
+            int c = key >> i & 1;
+            if (!ma.tree[node].ch[c]) {
+                int new_node = ma.new_node();
+                ma.tree[node].ch[c] = new_node;
+                ma.tree[ma.tree[node].ch[c]].par = node;
             }
+            node = ma.tree[node].ch[c];
         }
-        if (size[node]) return false;
-        size[node] = 1;
-        for (int i = 0; i < _bit; ++i) {
-            node = par[node];
-            size[node] += 1;
+        if (ma.tree[node].size) return false;
+        ma.tree[node].size = 1;
+        for (int i = 0; i < bit; ++i) {
+            node = ma.tree[node].par;
+            ma.tree[node].size++;
         }
         return true;
     }
@@ -87,125 +104,109 @@ struct BinaryTrieSet {
         return _find(key) != -1;
     }
 
-    void _discard(unsigned int node) {
-        for (int i = 0; i < _bit; ++i) {
-            size[node] -= 1;
-            if (left[par[node]] == node) {
-                node = par[node];
-                left[node] = 0;
-                if (right[node]) break;
-            } else {
-                node = par[node];
-                right[node] = 0;
-                if (left[node]) break;
-            }
+    void _discard(int node) {
+        for (int i = 0; i < bit; ++i) {
+            int c = ma.tree[ma.tree[node].par].ch[0] == node;
+            ma.tree[node].size--;
+            ma.erase(node);
+            node = ma.tree[node].par;
+            ma.tree[node].ch[c^1] = 0;
+            if (ma.tree[node].ch[c]) break;
         }
         while (node) {
-            size[node] -= 1;
-            node = par[node];
+            ma.tree[node].size--;
+            node = ma.tree[node].par;
         }
     }
 
     bool discard(T key) {
-        unsigned int node = _find(key);
+        int node = _find(key);
         if (node == -1) return false;
         _discard(node);
         return true;
     }
 
     T pop(int k) {
+        assert(0 <= k && k < len());
         if (k < 0) k += len();
-        int node = _root;
+        int node = root;
         T res = 0;
-        for (int i = _bit-1; i >= 0; --i) {
-            if ((_xor_val >> i) & 1) swap(left, right);
-            unsigned int t = size[left[node]];
+        for (int i = bit-1; i >= 0; --i) {
+            int c = xor_val >> i & 1;
+            int t = ma.tree[ma.tree[node].ch[c]].size;
             res <<= 1;
             if (t <= k) {
                 k -= t;
                 res |= 1;
-                node = right[node];
+                node = ma.tree[node].ch[c^1];
             } else {
-                node = left[node];
+                node = ma.tree[node].ch[c];
             }
-            if ((_xor_val >> i) & 1) swap(left, right);
         }
         _discard(node);
-        return res ^ _xor_val;
+        return res ^ xor_val;
     }
 
     T pop_min() {
+        assert(!empty());
         return pop(0);
     }
 
     T pop_max() {
-        return pop(-1);
+        assert(!empty());
+        return pop(len()-1);
     }
 
     void all_xor(T x) {
-        _xor_val ^= x;
+        xor_val ^= x;
     }
 
     T get_min() const {
-        T key = _xor_val;
+        T key = xor_val;
         T ans = 0;
-        unsigned int node = _root;
-        for (int i = _bit-1; i >= 0; --i) {
+        int node = root;
+        for (int i = bit-1; i >= 0; --i) {
             ans <<= 1;
-            if ((key >> i) & 1) {
-                if (right[node]) {
-                    node = right[node];
-                    ans |= 1;
-                } else {
-                    node = left[node];
-                }
+            int c = key >> i & 1;
+            if (ma.tree[node].ch[c]) {
+                node = ma.tree[node].ch[c];
+                ans |= c;
             } else {
-                if (left[node]) {
-                    node = left[node];
-                } else {
-                    node = right[node];
-                    ans |= 1;
-                }
+                node = ma.tree[node].ch[c^1];
+                ans |= (c^1);
             }
         }
-        return ans ^ _xor_val;
+        return ans ^ xor_val;
     }
 
     T get_max() const {
-        T key = _xor_val;
+        T key = xor_val;
         T ans = 0;
-        unsigned int node = _root;
-        for (int i = _bit-1; i >= 0; --i) {
+        int node = root;
+        for (int i = bit-1; i >= 0; --i) {
             ans <<= 1;
-            if ((key >> i) & 1) {
-                if (left[node]) {
-                    node = left[node];
-                } else {
-                    node = right[node];
-                    ans |= 1;
-                }
+            int c = key >> i & 1;
+            if (ma.tree[node].ch[c^1]) {
+                node = ma.tree[node].ch[c^1];
+                ans |= (c^1);
             } else {
-                if (right[node]) {
-                    ans |= 1;
-                    node = right[node];
-                } else {
-                    node = left[node];
-                }
+                node = ma.tree[node].ch[c];
+                ans |= c;
             }
         }
-        return ans ^ _xor_val;
+        return ans ^ xor_val;
     }
 
     int index(T key) const {
         int k = 0;
-        unsigned int node = _root;
-        key ^= _xor_val;
-        for (int i = _bit-1; i >= 0; --i) {
-            if ((key >> i) & 1) {
-                k += size[left[node]];
-                node = right[node];
+        int node = root;
+        key ^= xor_val;
+        for (int i = bit-1; i >= 0; --i) {
+            if (key >> i & 1) {
+                k += ma.tree[ma.tree[node].ch[0]].size;
+                node = ma.tree[node].ch[1];
             } else {
-                node = left[node];
+                node = ma.tree[node].ch[0];
             }
             if (!node) break;
         }
@@ -214,78 +215,96 @@ struct BinaryTrieSet {
 
     int index_right(T key) const {
         int k = 0;
-        unsigned int node = _root;
-        key ^= _xor_val;
-        for (int i = _bit-1; i >= 0; --i) {
-            if ((key >> i) & 1) {
-                k += size[left[node]];
-                node = right[node];
+        int node = root;
+        key ^= xor_val;
+        for (int i = bit-1; i >= 0; --i) {
+            if (key >> i & 1) {
+                k += ma.tree[ma.tree[node].ch[0]].size;
+                node = ma.tree[node].ch[1];
             } else {
-                node = left[node];
+                node = ma.tree[node].ch[0];
             }
             if (!node) break;
         }
-        if (node) k += 1;
+        if (node) k++;
         return k;
     }
 
-    T kth_elm(int k) {
-        if (k < 0) k += len();
-        unsigned int node = _root;
+    T get(int k) {
+        assert(0 <= k && k < len());
+        int node = root;
         T res = 0;
-        for (int i = _bit-1; i >= 0; --i) {
-            if ((_xor_val >> i) & 1) swap(left, right);
-            unsigned int t = size[left[node]];
-            res <<= 1;
-            if (t <= k) {
-                k -= t;
-                res |= 1;
-                node = right[node];
+        for (int i = bit-1; i >= 0; --i) {
+            if ((xor_val >> i) & 1) {
+                int t = ma.tree[ma.tree[node].ch[1]].size;
+                res <<= 1;
+                if (t <= k) {
+                    k -= t;
+                    res |= 1;
+                    node = ma.tree[node].ch[0];
+                } else {
+                    node = ma.tree[node].ch[1];
+                }
             } else {
-                node = left[node];
+                int t = ma.tree[ma.tree[node].ch[0]].size;
+                res <<= 1;
+                if (t <= k) {
+                    k -= t;
+                    res |= 1;
+                    node = ma.tree[node].ch[1];
+                } else {
+                    node = ma.tree[node].ch[0];
+                }
             }
-            if ((_xor_val >> i) & 1) swap(left, right);
         }
         return res;
     }
 
     T gt(T key) {
         int i = index_right(key);
-        return (i >= size[_root]? (-1) : kth_elm(i));
+        return (i >= ma.tree[root].size ? (-1) : get(i));
     }
 
     T lt(T key) {
         int i = index(key) - 1;
-        return (i < 0? -1 : kth_elm(i));
+        return (i < 0 ? -1 : get(i));
     }
 
     T ge(T key) {
-        if (key == 0) return (len()? get_min() : -1);
+        if (key == 0) return (len() ? get_min() : -1);
         int i = index_right(key - 1);
-        return (i >= size[_root]? -1 : kth_elm(i));
+        return (i >= ma.tree[root].size ? -1 : get(i));
     }
 
     T le(T key) {
         int i = index(key + 1) - 1;
-        return (i < 0? -1 : kth_elm(i));
+        return (i < 0 ? -1 : get(i));
     }
 
-    vector<T> tovector() {
+    vector<T> tovector() const {
         vector<T> a;
-        if (!len()) return a;
+        if (empty()) return a;
         a.reserve(len());
-        for (int i = 0; i < len(); ++i) {
-            T e = kth_elm(i);
-            a.emplace_back(e);
-        }
+        auto dfs = [&] (auto &&dfs, int d, int node, T now) -> void {
+            assert(node);
+            if (d < 0) {
+                assert(ma.tree[node].size);
+                a.emplace_back(now);
+                return;
+            }
+            int c = xor_val >> d & 1;
+            if (ma.tree[node].ch[c])   dfs(dfs, d-1, ma.tree[node].ch[c], now<<1|c);
+            if (ma.tree[node].ch[c^1]) dfs(dfs, d-1, ma.tree[node].ch[c^1], now<<1|(c^1));
+        };
+        dfs(dfs, bit-1, root, (T)0);
         return a;
     }
 
     bool empty() const {
-        return size[_root] == 0;
+        return ma.tree[root].size == 0;
     }
 
-    void print() {
+    void print() const {
         cout << "{";
         vector<T> a = tovector();
         for (int i = 0; i < len()-1; ++i) {
@@ -298,7 +317,7 @@ struct BinaryTrieSet {
     }
 
     int len() const {
-        return size[_root];
+        return ma.tree[root].size;
     }
 };
 }  // namespace titan23
