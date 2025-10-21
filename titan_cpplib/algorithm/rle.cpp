9c9958dc9856ac012f05d1901967fbe508c9bd9a
titan-23 <titan.kaneda@gmail.com>
2025-10-22 01:41:22 +0900

    Add rle

--- titan_cpplib/algorithm/rle.cpp
+++ titan_cpplib/algorithm/rle.cpp
@@ -1,25 +1,35 @@
 #include <vector>
+#include <string>
 using namespace std;
 
-// rle
 namespace titan23 {
 
 template<typename T>
-vector<pair<T, int>> rle(const vector<T> &a) {
-    if (a.empty()) return {};
-    int n = (int)a.size();
+vector<pair<T, int>> RLE(const vector<T> &a) {
     vector<pair<T, int>> res;
-    T now = a[0];
-    res.emplace_back(now, 1);
-    for (int i = 1; i < n; ++i) {
-        if (a[i] == now) {
+    if (a.empty()) return res;
+    res.emplace_back(a[0], 1);
+    for (int i = 1; i < (int)a.size(); ++i) {
+        if (res.back().first == a[i]) {
             res.back().second++;
         } else {
             res.emplace_back(a[i], 1);
-            now = a[i];
         }
     }
     return res;
 }
 
-}  // namespace titan23
+vector<pair<char, int>> RLE(const string &s) {
+    vector<pair<char, int>> res;
+    if (s.empty()) return res;
+    res.emplace_back(s[0], 1);
+    for (int i = 1; i < (int)s.size(); ++i) {
+        if (res.back().first == s[i]) {
+            res.back().second++;
+        } else {
+            res.emplace_back(s[i], 1);
+        }
+    }
+    return res;
+}
+} // namespace titan23
