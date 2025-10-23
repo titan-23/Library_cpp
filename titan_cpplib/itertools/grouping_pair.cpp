fa06ab41e94d23fdcf7c45b8416f27217f76ae5b
titan-23 <titan.kaneda@gmail.com>
2025-10-23 11:55:28 +0900

    Add iter

--- titan_cpplib/itertools/grouping_pair.cpp
+++ titan_cpplib/itertools/grouping_pair.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // 2N個のものをN個のペアに分ける方法
 // (2*n-1) * (2*n-3) * (2*n-5) * ... * 1
-vector<vector<pair<int, int>>> get_2n_to_n_pair(int n) {
+vector<vector<pair<int, int>>> grouping_pair(int n) {
     vector<vector<pair<int, int>>> result;
     vector<pair<int, int>> tmp;
     vector<bool> is_used(n, false);
