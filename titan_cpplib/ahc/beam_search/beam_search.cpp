f36ca24fb5700f5f4b9c6e68bf21176920779562
titan-23 <titan.kaneda@gmail.com>
2026-02-04 14:51:29 +0900

    重複除去機能を追加

--- titan_cpplib/ahc/beam_search/beam_search.cpp
+++ titan_cpplib/ahc/beam_search/beam_search.cpp
@@ -4,12 +4,13 @@
 #include <vector>
 #include <cassert>
 #include <algorithm>
+#include <tuple>
 
+#include "titan_cpplib/others/print.cpp"
 #include "titan_cpplib/algorithm/random.cpp"
 #include "titan_cpplib/ahc/state_pool.cpp"
 #include "titan_cpplib/ahc/timer.cpp"
-#include "titan_cpplib/data_structures/hash_set.cpp"
-#include "titan_cpplib/others/print.cpp"
+#include "titan_cpplib/data_structures/hash_dict.cpp"
 #include "titan_cpplib/ahc/beam_search/beam_param.cpp"
 
 using namespace std;
@@ -17,7 +18,7 @@ using namespace std;
 //! 木上のビームサーチライブラリ
 namespace flying_squirrel {
 
-template<typename ScoreType, typename HashType, typename Action, typename State>
+template<typename ScoreType, typename HashType, class Action, class State>
 class BeamSearchWithTree {
     // ref: https://eijirou-kyopro.hatenablog.com/entry/2024/02/01/115639
 
@@ -26,7 +27,6 @@ private:
     static constexpr const int POST_ORDER = -2;
     titan23::Random rnd;
     titan23::Timer beam_timer;
-    titan23::HashSet seen;
     using ActionIDType = int;
     ActionIDType ActionID;
     vector<Action> result;
@@ -36,12 +36,112 @@ private:
     // <dir or id, action, action_id>
     // dir or id := 葉のとき、leaf_id
     //              そうでないとき、行きがけなら-1、帰りがけなら-2
-    vector<tuple<int, Action, ActionIDType>> tree;
+    vector<tuple<int, Action, ActionIDType>> tree, nxt_tree;
 
-    // 次のビーム候補を保持する配列
-    vector<tuple<int, int, ScoreType, Action, ActionIDType>> next_beam; // <par, ch_idx, score, action, action_id>
+    class Candidates {
+    private:
+        using T = pair<ScoreType, int>;
+        vector<HashType> hashidx;
+        titan23::HashDict<int> func;
+        int beam_width, entry;
+        int s = 1;
+        vector<T> seg;
 
-    vector<vector<int>> next_beam_data;
+        void set(int k, T v) {
+            k += s;
+            seg[k] = v;
+            while (k > 1) {
+                k >>= 1;
+                seg[k] = seg[k<<1].first > seg[k<<1|1].first ? seg[k<<1] : seg[k<<1|1];
+            }
+        }
+
+    public:
+        // 次のビーム候補を保持する配列
+        vector<tuple<int, ScoreType, Action, ActionIDType>> next_beam; // <par, score, action, action_id>
+
+        Candidates() {}
+
+        int size() const {
+            return entry;
+        }
+
+        /// @brief 現在のworstを返す / これ以上なら意味ない
+        ScoreType threshold() const {
+            return entry < beam_width ? numeric_limits<ScoreType>::max() : seg[1].first;
+        }
+
+        /// 追加できたらtrueを返す
+        bool push(
+            ScoreType score, HashType hash,
+            int par, const Action &action, ActionIDType action_id
+        ) {
+            if (entry == beam_width && score >= seg[1].first) {
+                return false;
+            }
+            auto dat = func.get_pos(hash);
+            int idx = func.inner_get(dat, -1);
+            if (idx != -1) {
+                if (score < seg[idx+s].first) {
+                    next_beam[idx] = {par, score, move(action), action_id};
+                    set(idx, {score, idx});
+                    return true;
+                }
+                return false;
+            }
+            if (entry < beam_width) {
+                func.inner_set(dat, hash, entry);
+                next_beam[entry] = {par, score, move(action), action_id};
+                hashidx[entry] = hash;
+                set(entry, {score, entry});
+                entry++;
+                return true;
+            }
+            auto [_, i] = seg[1];
+            next_beam[i] = {par, score, move(action), action_id};
+            func.set(hashidx[i], -1);
+            func.inner_set(dat, hash, i);
+            hashidx[i] = hash;
+            set(i, {score, i});
+            return true;
+        }
+
+        void reset(int w) {
+            beam_width = w;
+            while (s < w) {
+                s <<= 1;
+            }
+            if (seg.size() < 2*s) {
+                seg.resize(2*s);
+            }
+            fill(seg.begin(), seg.begin()+(2*w), make_pair(numeric_limits<ScoreType>::lowest(), -1));
+            if (hashidx.size() < w) {
+                hashidx.resize(w);
+                next_beam.resize(w);
+            }
+            func.clear();
+            if (func.inner_len() == 1) {
+                func = titan23::HashDict<int>(beam_width*8);
+            }
+            entry = 0;
+        }
+
+        void shuffle(titan23::Random &rnd) {
+            // shuffle
+            for (int i = 0; i < entry-1; ++i) {
+                int j = rnd.randrange(i, entry);
+                swap(next_beam[i], next_beam[j]);
+            }
+        }
+
+        tuple<int, ScoreType, Action, ActionIDType> get_best() {
+            return *min_element(next_beam.begin(), next_beam.begin() + entry, [&] (const tuple<int, ScoreType, Action, ActionIDType> &left, const tuple<int, ScoreType, Action, ActionIDType> &right) {
+                return std::get<1>(left) < std::get<1>(right);
+            });
+        }
+    } candidates;
+
+    vector<Action> actions;
 
     /**
      * @brief 次のビームを求める
@@ -51,19 +151,14 @@ private:
      * @param turn 謎
      */
     void get_next_beam(State* state, const int t_turn, const int turn) {
-        next_beam.clear();
-        next_beam.reserve(tree.size());
-        seen.clear();
-
         if (turn == 0) {
-            vector<Action> actions = state->get_actions(t_turn, (result.empty() ? DAMMY_ACTION : result.back()));
-            int idx = 0;
+            actions.clear();
+            state->get_actions(actions, t_turn, (result.empty() ? DAMMY_ACTION : result.back()));
             for (Action &action : actions) {
-                auto [score, hash] = state->try_op(action);
-                if (seen.contains_insert(hash)) continue;
-                next_beam.emplace_back(PRE_ORDER, idx, score, action, ActionID);
-                idx++;
-                ActionID++;
+                auto [score, hash] = state->try_op(action, candidates.threshold());
+                if (candidates.push(score, hash, PRE_ORDER, move(action), ActionID)) {
+                    ActionID++;
+                }
             }
             return;
         }
@@ -72,37 +167,36 @@ private:
         for (int i = 0; i < tree.size(); ++i) {
             auto &[dir_or_leaf_id, action, _] = tree[i];
             if (dir_or_leaf_id >= 0) {
-                state->apply_op(action);
-                vector<Action> actions = state->get_actions(t_turn, action);
+                state->apply_op(move(action));
+                actions.clear();
+                state->get_actions(actions, t_turn, action);
                 std::get<0>(tree[i]) = leaf_id;
-                int idx = 0;
                 for (Action &action : actions) {
-                    auto [score, hash] = state->try_op(action);
-                    if (seen.contains_insert(hash)) continue;
-                    next_beam.emplace_back(leaf_id, idx, score, action, ActionID);
-                    idx++;
-                    ActionID++;
+                    auto [score, hash] = state->try_op(action, candidates.threshold());
+                    if (candidates.push(score, hash, leaf_id, move(action), ActionID)) {
+                        ActionID++;
+                    }
                 }
                 ++leaf_id;
-                state->rollback(action);
+                state->rollback(move(action));
             } else if (dir_or_leaf_id == PRE_ORDER) {
-                state->apply_op(action);
+                state->apply_op(move(action));
             } else {
-                state->rollback(action);
+                state->rollback(move(action));
             }
         }
     }
 
     //! 不要なNodeを削除し、木を更新する
     int update_tree(State* state, const int turn) {
-        vector<tuple<int, Action, ActionIDType>> new_tree;
-        new_tree.reserve(tree.size());
+        nxt_tree.clear();
         if (turn == 0) {
-            for (const auto &[par, _, __, new_action, action_id] : next_beam) {
+            for (int i = 0; i < candidates.size(); ++i) {
+                const auto &[par, _, new_action, action_id] = candidates.next_beam[i];
                 assert(par == -1);
-                new_tree.emplace_back(0, new_action, action_id);
+                nxt_tree.emplace_back(0, move(new_action), action_id);
             }
-            swap(tree, new_tree);
+            swap(tree, nxt_tree);
             return 0;
         }
 
@@ -122,36 +216,47 @@ private:
             }
         }
 
+        int next_beam_idx = 0;
+        const int num_candidates = candidates.size();
         for (; i < tree.size(); ++i) {
             auto &[dir_or_leaf_id, action, action_id] = tree[i];
             if (dir_or_leaf_id >= 0) {
-                if (next_beam_data[dir_or_leaf_id].empty()) continue;
-                new_tree.emplace_back(PRE_ORDER, action, action_id);
-                for (int beam_idx : next_beam_data[dir_or_leaf_id]) {
-                    auto &[_, __, ___, new_action, new_action_id] = next_beam[beam_idx];
-                    new_tree.emplace_back(dir_or_leaf_id, std::move(new_action), new_action_id);
+                bool has_child = false;
+                int start_idx = next_beam_idx;
+                while(next_beam_idx < num_candidates) {
+                    if (std::get<0>(candidates.next_beam[next_beam_idx]) != dir_or_leaf_id) {
+                        break;
+                    }
+                    has_child = true;
+                    next_beam_idx++;
                 }
-                new_tree.emplace_back(POST_ORDER, action, action_id);
-                next_beam_data[dir_or_leaf_id].clear();
+                if (!has_child) continue;
+                nxt_tree.emplace_back(PRE_ORDER, move(action), action_id);
+                for (int k = start_idx; k < next_beam_idx; ++k) {
+                    auto &[_, __, new_action, new_action_id] = candidates.next_beam[k];
+                    nxt_tree.emplace_back(dir_or_leaf_id, move(new_action), new_action_id);
+                }
+                nxt_tree.emplace_back(POST_ORDER, move(action), action_id);
             } else if (dir_or_leaf_id == PRE_ORDER) {
-                new_tree.emplace_back(PRE_ORDER, std::move(action), action_id);
+                nxt_tree.emplace_back(PRE_ORDER, move(action), action_id);
             } else {
-                int pre_dir = std::get<0>(new_tree.back());
+                int pre_dir = std::get<0>(nxt_tree.back());
                 if (pre_dir == PRE_ORDER) {
-                    new_tree.pop_back(); // 一つ前が行きがけなら、削除して追加しない
+                    nxt_tree.pop_back(); // 一つ前が行きがけなら、削除して追加しない
                 } else {
-                    new_tree.emplace_back(POST_ORDER, std::move(action), action_id);
+                    nxt_tree.emplace_back(POST_ORDER, move(action), action_id);
                 }
             }
         }
-        swap(tree, new_tree);
+        swap(tree, nxt_tree);
         return apply_only_turn;
     }
 
     void get_result() {
         int best_id = -1;
         ScoreType best_score = 0;
-        for (const auto &[par, ch_idx, score, _, __] : next_beam) {
+        for (int i = 0; i < candidates.size(); ++i) {
+            const auto &[par, score, _, __] = candidates.next_beam[i];
             if (best_id == -1 || score < best_score) {
                 best_score = score;
                 best_id = par;
@@ -161,10 +266,11 @@ private:
             int best_ch_idx = -1;
             Action best_action;
             ScoreType best_score = 0;
-            for (const auto &[_, ch_idx, score, action, __] : next_beam) {
+            for (int i = 0; i < candidates.size(); ++i) {
+                const auto &[_, score, action, __] = candidates.next_beam[i];
                 if (best_ch_idx == -1 || score < best_score) {
                     best_score = score;
-                    best_ch_idx = ch_idx;
+                    best_ch_idx = 1;
                     best_action = action;
                 }
             }
@@ -191,7 +297,6 @@ private:
     void init_bs(const BeamParam &param) {
         beam_timer.reset();
         rnd = titan23::Random();
-        this->seen = titan23::HashSet(param.beam_width*8); // TODO
         ActionID = 0;
         result.clear();
     }
@@ -213,49 +318,44 @@ public:
         int now_turn = 0;
         for (int turn = 0; turn < param.max_turn; ++turn) {
             double now_time = beam_timer.elapsed();
-            // if (verbose) cerr << "\nInfo: # turn : " << turn+1 << " | " << now_time << " ms" << endl;
+            if (verbose) cerr << "\nInfo: # turn : " << turn+1 << " | " << now_time << " ms" << endl;
 
             // 次のビーム候補を求める
+            int w = param.get_beam_width(param.max_turn-turn, tree.size(), param.time_limit-beam_timer.elapsed());
+            candidates.reset(w);
             get_next_beam(state, turn, turn-now_turn);
-            rnd.shuffle(next_beam); // シャッフルして多様性を確保(できたらいいな)
+            candidates.shuffle(rnd);
 
-            if (next_beam.empty()) {
+            if (candidates.size() == 0) {
                 cerr << to_red("Error: \t次の候補が見つかりませんでした") << endl;
-                assert(!next_beam.empty());
+                assert(candidates.size() > 0);
             }
 
-            // ビームを絞る
-            int beam_width = min(param.get_beam_width(param.max_turn-turn, tree.size(), param.time_limit-beam_timer.elapsed()), (int)next_beam.size());
-            nth_element(next_beam.begin(), next_beam.begin() + beam_width, next_beam.end(), [&] (const tuple<int, int, ScoreType, Action, ActionIDType> &left, const tuple<int, int, ScoreType, Action, ActionIDType> &right) {
-                return std::get<2>(left) < std::get<2>(right);
-            });
+            tuple<int, ScoreType, Action, ActionIDType> bests = candidates.get_best();
 
-            tuple<int, int, ScoreType, Action, ActionIDType> bests = *min_element(next_beam.begin(), next_beam.begin() + beam_width, [&] (const tuple<int, int, ScoreType, Action, ActionIDType> &left, const tuple<int, int, ScoreType, Action, ActionIDType> &right) {
-                return std::get<2>(left) < std::get<2>(right);
-            });
-            // if (verbose) cerr << "Info: \tbest_score = " << std::get<2>(bests) << endl;
-            if (std::get<2>(bests) == 0) { // TODO 終了条件:ベストスコアを元に判定している
+            if (verbose) cerr << "Info: \tbest_score = " << std::get<1>(bests) << endl;
+            if (std::get<1>(bests) == 0) { // TODO 終了条件
                 cerr << to_green("Info: find valid solution.") << endl;
+                candidates.next_beam.clear();
+                candidates.next_beam.push_back(bests);
                 get_result();
-                result.emplace_back(std::get<3>(bests));
+                result.emplace_back(std::get<2>(candidates.next_beam[0]));
                 if (verbose) param.report();
                 return result;
             }
 
             // 探索木の更新
             if (turn != 0) {
-                if (next_beam_data.size() < next_beam.size()) {
-                    next_beam_data.resize(next_beam.size());
-                }
-                for (int i = 0; i < beam_width; ++i) {
-                    auto &[par, _, __, new_action, new_action_id] = next_beam[i];
-                    next_beam_data[par].emplace_back(i);
-                }
+                sort(candidates.next_beam.begin(), candidates.next_beam.begin() + candidates.size(),
+                    [] (const auto& a, const auto& b) {
+                        return std::get<0>(a) < std::get<0>(b);
+                    }
+                );
             }
             int apply_only_turn = update_tree(state, turn);
             now_turn += apply_only_turn;
 
-            param.timestamp(tree.size(), beam_width, beam_timer.elapsed()-now_time);
+            param.timestamp(tree.size(), candidates.size(), beam_timer.elapsed()-now_time);
         }
 
         // 答えを復元する
