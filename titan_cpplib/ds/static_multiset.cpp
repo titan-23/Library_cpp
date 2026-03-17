64f3e4eb4ba4585d458e25338089139eed4f8e0c
titan-23 <titan.kaneda@gmail.com>
2026-03-18 01:30:49 +0900

    Fix bug

--- titan_cpplib/ds/static_multiset.cpp
+++ titan_cpplib/ds/static_multiset.cpp
@@ -13,8 +13,8 @@ private:
     int n;
 
 public:
-    StaticMultiset() {}
-    StaticMultiset(T missing) : missing(missing) {}
+    StaticMultiset() : missing(-1), n(0) {}
+    StaticMultiset(T missing) : missing(missing), n(0) {}
     StaticMultiset(vector<T> &a, T missing) : data(a), missing(missing) {
         sort(data.begin(), data.end());
         n = (int)data.size();
@@ -32,71 +32,48 @@ public:
 
     //! 昇順 `k` 番目の要素を返す / `O(1)`
     T get(const int i) const {
+        assert(0 <= i && i < len());
         return data[i];
     }
 
     //! `key` 以上で最小 / `O(logn)`
     T ge(const T &key) const {
-        if (key > data.back() || empty()) return missing;
-        int l = -1, r = n-1;
-        while (r - l > 1) {
-            int mid = (l + r) >> 1;
-            (data[mid] >= key ? r : l) = mid;
-        }
-        return data[r];
+        auto it = lower_bound(data.begin(), data.end(), key);
+        if (it == data.end()) return missing;
+        return *it;
     }
 
     //! `key` より大きくて最小 / `O(logn)`
     T gt(const T &key) const {
-        if (key >= data.back() || empty()) return missing;
-        int l = -1, r = n-1;
-        while (r - l > 1) {
-            int mid = (l + r) >> 1;
-            (data[mid] > key ? r : l) = mid;
-        }
-        return data[r];
+        auto it = upper_bound(data.begin(), data.end(), key);
+        if (it == data.end()) return missing;
+        return *it;
     }
 
     //! `key` 以下で最大 / `O(logn)`
     T le(const T &key) const {
-        if (key < data[0] || empty()) return missing;
-        int l = 0, r = n;
-        while (r - l > 1) {
-            int mid = (l + r) >> 1;
-            (data[mid] <= key ? l : r) = mid;
-        }
-        return data[l];
+        auto it = upper_bound(data.begin(), data.end(), key);
+        if (it == data.begin()) return missing;
+        return *(--it);
     }
 
     //! `key` 未満で最大 / `O(logn)`
     T lt(const T &key) const {
-        if (key <= data[0] || empty()) return missing;
-        int l = 0, r = n;
-        while (r - l > 1) {
-            int mid = (l + r) >> 1;
-            (data[mid] < key ? l : r) = mid;
-        }
-        return data[l];
+        auto it = lower_bound(data.begin(), data.end(), key);
+        if (it == data.begin()) return missing;
+        return *(--it);
     }
 
     //! `upper` 未満の要素数を返す / `O(logn)`
     int index(const T &upper) const {
-        int l = -1, r = n;
-        while (r - l > 1) {
-            int mid = (l + r) >> 1;
-            (data[mid] < upper ? l : r) = mid;
-        }
-        return r;
+        auto it = lower_bound(data.begin(), data.end(), upper);
+        return distance(data.begin(), it);
     }
 
     //! `upper` 以下の要素数を返す / `O(logn)`
     int index_right(const T &upper) const {
-        int l = -1, r = n;
-        while (r - l > 1) {
-            int mid = (l + r) >> 1;
-            (data[mid] <= upper ? l : r) = mid;
-        }
-        return r;
+        auto it = upper_bound(data.begin(), data.end(), upper);
+        return distance(data.begin(), it);
     }
 
     //! `key` の要素数を返す / `O(logn)`
@@ -110,13 +87,10 @@ public:
         return index(upper) - index(lower);
     }
 
-    //! `key` の存在判定 / `o(logn)`
+    //! `key` の存在判定 / `O(logn)`
     bool contains(const T &key) const {
-        int idx = index(key);
-        if (idx == (int)data.size()) {
-            return false;
-        }
-        return data[idx] == key;
+        auto it = lower_bound(data.begin(), data.end(), key);
+        return it != data.end() && *it == key;
     }
 
     friend ostream& operator<<(ostream& os, const titan23::StaticMultiset<T> &s) {
