16a8bdd30853bbb6adbc54cc031661f09f1e55b1
titan-23 <titan.kaneda@gmail.com>
2026-06-30 23:00:14 +0900

    江

--- titan_cpplib/alg/doubling.cpp
+++ titan_cpplib/alg/doubling.cpp
@@ -1,11 +1,16 @@
+#pragma once
+
+#include <vector>
+#include <cassert>
+using namespace std;
+
 namespace titan23 {
 
 /// @brief Doubling
-template <class T, T (*op)(T, T), T (*e)()>
 class Doubling {
 private:
     int n, log;
-    vector<vector<pair<int, T>>> db;
+    vector<vector<int>> db;
 
     int bit_length(long long LIM) {
         if (LIM == 0) return 0;
@@ -15,36 +20,32 @@ private:
 public:
     /// @param LIM kthの最大値
     /// @param A A[i]:=iからの遷移先
-    /// @param B B[i]:=iからの遷移で加算される値
-    Doubling(long long LIM, const vector<int> &A, const vector<T> &B) : n(A.size()), log(bit_length(LIM)) {
-        db.resize(log+1, vector<pair<int, T>>(n, {-1, e()}));
+    Doubling(long long LIM, const vector<int> &A) : n(A.size()), log(bit_length(LIM)) {
+        db.resize(log+1, vector<int>(n, -1));
         for (int k = 0; k < n; ++k) {
-            db[0][k] = {A[k], B[k]};
+            db[0][k] = A[k];
         }
         for (int k = 0; k < log; ++k) {
             for (int i = 0; i < n; ++i) {
-                if (db[k][i].first == -1) {
-                    db[k+1][i] = db[k][i];
+                if (db[k][i] == -1) {
+                    db[k+1][i] = -1;
                 } else {
-                    db[k+1][i].first = db[k][db[k][i].first].first;
-                    db[k+1][i].second = op(db[k][i].second, db[k][db[k][i].first].second);
+                    db[k+1][i] = db[k][db[k][i]];
                 }
             }
         }
     }
 
-    /// @brief startからk回遷移したときの{位置、モノイド積}
-    pair<int, T> kth(int start, long long k) const {
+    /// @brief startからk回遷移したときの位置
+    int kth(int start, long long k) const {
         assert(0 <= start && start < n);
-        T res = e();
         for (int i = log-1; i >= 0; --i) {
             if (k >> i & 1) {
-                res = op(res, db[i][start].second);
-                start = db[i][start].first;
+                start = db[i][start];
             }
             if (start == -1) break;
         }
-        return {start, res};
+        return start;
     }
 };
 } // namespace titan23
