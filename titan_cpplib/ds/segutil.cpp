6b9b47ee46b5ba20458c3c2f7d86fd4eea885e1c
titan-23 <titan.kaneda@gmail.com>
2026-07-14 19:10:34 +0900

    y

--- titan_cpplib/ds/segutil.cpp
+++ titan_cpplib/ds/segutil.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <numeric>
 #include <limits>
+#include <utility>
 #include "titan_cpplib/ds/segment_tree.cpp"
 using namespace std;
 
@@ -20,6 +21,49 @@ template<typename T> T e_sum() { return 0; }
 
 template<typename T> T op_gcd(T s, T t) { return gcd(s, t); }
 template<typename T> T e_gcd() { return 0; }
+
+template<typename T> T op_xor(T s, T t) { return s ^ t; }
+template<typename T> T e_xor() { return 0; }
+
+template<typename T> T op_or(T s, T t) { return s | t; }
+template<typename T> T e_or() { return 0; }
+
+template<typename T> T op_and(T s, T t) { return s & t; }
+template<typename T> T e_and() { return ~T(0); }
+
+// (値, 添字)。値が同じときは添字の小さい方をとる
+template<typename T> pair<T, int> op_min_idx(pair<T, int> s, pair<T, int> t) { return min(s, t); }
+template<typename T> pair<T, int> e_min_idx() { return {numeric_limits<T>::max(), -1}; }
+
+template<typename T> pair<T, int> op_max_idx(pair<T, int> s, pair<T, int> t) {
+    if (s.first != t.first) return s.first > t.first ? s : t;
+    return s.second <= t.second ? s : t;
+}
+template<typename T> pair<T, int> e_max_idx() { return {numeric_limits<T>::lowest(), -1}; }
+
+// (値, 個数)
+template<typename T> pair<T, int> op_min_cnt(pair<T, int> s, pair<T, int> t) {
+    if (s.first != t.first) return s.first < t.first ? s : t;
+    return {s.first, s.second + t.second};
+}
+template<typename T> pair<T, int> e_min_cnt() { return {numeric_limits<T>::max(), 0}; }
+
+template<typename T> pair<T, int> op_max_cnt(pair<T, int> s, pair<T, int> t) {
+    if (s.first != t.first) return s.first > t.first ? s : t;
+    return {s.first, s.second + t.second};
+}
+template<typename T> pair<T, int> e_max_cnt() { return {numeric_limits<T>::lowest(), 0}; }
+
+// 一次関数 f(x) = ax + b の合成
+template<typename T>
+struct Affine {
+    T a, b;
+    T eval(T x) const { return a * x + b; }
+};
+template<typename T> Affine<T> op_affine(Affine<T> s, Affine<T> t) {
+    return {t.a * s.a, t.a * s.b + t.b};
+}
+template<typename T> Affine<T> e_affine() { return {T(1), T(0)}; }
 
 // 区間の最大部分配列和(空区間を許す)を管理するモノイド
 template<typename T>
@@ -43,20 +87,56 @@ template<typename T> MaxSubSum<T> op_max_sub_sum(MaxSubSum<T> a, MaxSubSum<T> b)
 
 template<typename T> MaxSubSum<T> e_max_sub_sum() { return MaxSubSum<T>(); }
 
+// 区間 gcd
 template<typename T>
 using SegGcd = titan23::SegmentTree<T, op_gcd<T>, e_gcd<T>>;
 
+// 区間 max
 template<typename T>
 using SegMax = titan23::SegmentTree<T, op_max<T>, e_max<T>>;
 
+// 区間 min
 template<typename T>
 using SegMin = titan23::SegmentTree<T, op_min<T>, e_min<T>>;
 
+// 区間和
 template<typename T>
 using SegSum = titan23::SegmentTree<T, op_sum<T>, e_sum<T>>;
 
 // prod(l, r).ans が区間 [l, r) の最大部分配列和
 template<typename T>
 using SegMaxSubSum = titan23::SegmentTree<MaxSubSum<T>, op_max_sub_sum<T>, e_max_sub_sum<T>>;
+
+// 区間 xor
+template<typename T>
+using SegXor = titan23::SegmentTree<T, op_xor<T>, e_xor<T>>;
+
+// 区間 or
+template<typename T>
+using SegOr = titan23::SegmentTree<T, op_or<T>, e_or<T>>;
+
+// 区間 and
+template<typename T>
+using SegAnd = titan23::SegmentTree<T, op_and<T>, e_and<T>>;
+
+// (区間min, その添字) を返す / 要素は {値, 添字} で持つ
+template<typename T>
+using SegMinIdx = titan23::SegmentTree<pair<T, int>, op_min_idx<T>, e_min_idx<T>>;
+
+// (区間max, その添字) を返す / 要素は {値, 添字} で持つ
+template<typename T>
+using SegMaxIdx = titan23::SegmentTree<pair<T, int>, op_max_idx<T>, e_max_idx<T>>;
+
+// (区間min, その個数) を返す / 要素は {値, 1} で持つ
+template<typename T>
+using SegMinCnt = titan23::SegmentTree<pair<T, int>, op_min_cnt<T>, e_min_cnt<T>>;
+
+// (区間max, その個数) を返す / 要素は {値, 1} で持つ
+template<typename T>
+using SegMaxCnt = titan23::SegmentTree<pair<T, int>, op_max_cnt<T>, e_max_cnt<T>>;
+
+// 一次関数合成 / prod(l, r).eval(x) が f_{r-1}(...f_l(x)...) を返す
+template<typename T>
+using SegAffine = titan23::SegmentTree<Affine<T>, op_affine<T>, e_affine<T>>;
 }
 } // namespace titan23
